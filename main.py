c1e643546ca00d0c3901108043b7225463e75355
webik2019 <webik2019@gmail.com>
2019-01-07 13:34:39 +0000

    cats yay!

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<h1>Hello, World!</h1>'
+
+@app.route("/cat")
+def cat():
+    return '<blockquote class="reddit-card" data-card-created="1546786759"><a href="https://www.reddit.com/r/funny/comments/aczfgr/ohmygodthisisthebestthingeva/">"OHMYGODTHISISTHEBESTTHINGEVA"</a> from <a href="http://www.reddit.com/r/funny">r/funny</a></blockquote> <script async src="//embed.redditmedia.com/widgets/platform.js" charset="UTF-8"></script>'
